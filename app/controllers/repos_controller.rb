6bde9cf26ccdb6c756bc46c01ee39309e7cd3bc7
Kiril <kiril@gitbutler.com>
2026-03-28 21:58:50 +0000

    feat: show commit history on repo page, fix clone URLs to use request host

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -3,6 +3,7 @@ class ReposController < ApplicationController
     @owner = params[:owner]
     @repo = Repo.find_by!(owner: @owner, name: params[:repo])
     @readme = fetch_readme(@repo)
+    @commits = fetch_commits(@repo)
     @base_url = "#{request.protocol}#{request.host_with_port}"
   rescue ActiveRecord::RecordNotFound
     render status: :not_found, plain: "Repo not found"
@@ -12,10 +13,20 @@ class ReposController < ApplicationController
 
   def fetch_readme(repo)
     return nil unless Dir.exist?(repo.disk_path)
-    # Try to read README.md from the bare repo
     result = `git --git-dir=#{Shellwords.escape(repo.disk_path)} show HEAD:README.md 2>/dev/null`
     $?.success? ? result : nil
   rescue
     nil
+  end
+
+  def fetch_commits(repo)
+    return [] unless Dir.exist?(repo.disk_path)
+    output = `git --git-dir=#{Shellwords.escape(repo.disk_path)} log --pretty=format:"%h|%s|%an|%ar" -8 2>/dev/null`
+    output.lines.map do |line|
+      hash, subject, author, time = line.chomp.split("|", 4)
+      { hash: hash, subject: subject, author: author, time: time }
+    end
+  rescue
+    []
   end
 end
